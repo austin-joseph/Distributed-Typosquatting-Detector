1114b2573632216c90df2efabf71039b5932b9e6
Xiangshuai Gao <xiangshuai.gao@stonybrook.edu>
2019-11-02 00:35:19 -0400

    finished the front end javascript

---
+++ src/static/js/new_index.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', init);
+const timerID = {};
+
+function init() {
+    window.dummy_url = $('#dummy_url');
+    window.dummy_url.detach();
+    $('#submit_btn').click(submit_handler);
+}
+
+function submit_handler(event){
+    let url = $('#search').val();
+    if(url === '') {
+        alert('You need to enter a url first');
+        return;
+    }
+    let new_url = window.dummy_url.clone();
+    new_url.attr("id", url);
+    new_url.html(new_url.html().replace("dummy.com", url));
+    new_url.find('span').hide();
+    $('#url_list').append(new_url);
+    timerID[url] = setInterval(() => submit_url(url), 2000);
+}
+
+async function submit_url(url){
+    try{
+        let form_data = new FormData();
+        form_data.append("url", url);
+
+        let response = await fetch('/view',{
+            method: "POST",
+            body: form_data
+        });
+
+        let response_json = await response.json();
+        console.log(response_json.done);
+        if(!response_json.done){
+            return;
+        }
+        $(`#${url}`).find('span').show();
+        clearInterval(timerID[url]);
+    }catch (err) {
+        alert(`Encounters this message ${err.message}`);
+    }
+}
