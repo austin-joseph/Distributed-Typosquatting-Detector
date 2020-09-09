15823b4ea1aa2452e98119523056130151e3dee7
Austin <austinobejo@gmail.com>
2020-09-08 20:01:17 -0400

    Add local debug port

--- docker-mysql/build.sh
+++ docker-mysql/build.sh
@@ -1,2 +1,2 @@
 docker build -t dtd-mysql .
-docker create --name dtd-mysql -e MYSQL_ROOT_PASSWORD=mysql --network dtd-network -v /var/lib/mysql:/dtd-mysql/datadir dtd-mysql
+docker create -p 3360:3360 --name dtd-mysql -e MYSQL_ROOT_PASSWORD=mysql --network dtd-network -v /var/lib/mysql:/dtd-mysql/datadir dtd-mysql
